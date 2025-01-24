796e528e4801391a9f2104b95d6e62863dbdefb0
DaniloPinheiro91 <dpinheiro.profissional@hotmail.com>
2025-01-24 06:47:05 -0300

    Add files via upload

---
+++ Prova Algoritmos e Variáveis.py
@@ -0,0 +1,8 @@
+#calculando a Area de um retângulo
+base = float(input('Insira a base do triângulo a ser calculado:'))
+print (f'A base enviada foi: {base}')
+altura = float(input('Insira a Altura do triângulo a ser calculado'))
+print (f'A altura enviada foi:{altura}')
+
+area = base * altura
+print (f'A Area do Triangulo calculada é de: {area}')
