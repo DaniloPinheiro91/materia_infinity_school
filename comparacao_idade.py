7c3385903aa30003258c18f03b8fe056cb5d1395
DaniloPinheiro91 <dpinheiro.profissional@hotmail.com>
2025-01-24 18:00:18 -0300

    comparador de idade
    o usuário informa duas idade e o o comparador verifica se a idade é maior, menor ou igual.

---
+++ comparacao_idade.py
@@ -0,0 +1,9 @@
+idade1 = int(input('Informe a primeira idade para a comparação: '))
+idade2 = int(input('Informe a segunda idade para a comparação: '))
+
+if idade1 > idade2:
+    print ('Primeira idade é maior que a segunda idade informada')
+elif idade1 < idade2:
+    print ('A segunda idade é maior que a primeira idade informada')
+elif idade1 == idade2:
+    print('As idades informadas são iguais')    
