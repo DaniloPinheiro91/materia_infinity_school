cfc32502c7c59fcdd7361be95d766a0a7894c550
DaniloPinheiro91 <dpinheiro.profissional@hotmail.com>
2025-01-29 07:51:39 -0300

    Contagem Regressiva:
    Desenvolva um programa que use um laço while para exibir
    uma contagem regressiva de 10 até 1 e, em seguida, exiba
    "Feliz Ano Novo!".

---
+++ contagem regressiva com while.py
@@ -0,0 +1,7 @@
+contagem_regressiva = 10
+
+while contagem_regressiva >= 0:
+    print(f' faltam {contagem_regressiva} segundos Para o Ano novo')
+    contagem_regressiva -= 1
+
+print ('Feliz Ano Novo! ')
