a0face8e6e32e2da8187bb3be04a28644e20dda8
DaniloPinheiro91 <dpinheiro.profissional@hotmail.com>
2025-01-29 06:43:08 -0300

    Prova 2 - Condicionais
    Crie um programa em Python para verificar se um número é positivo, negativo ou zero. O programa deve solicitar ao usuário que insira um número e, em seguida, imprimir uma mensagem indicando a natureza do número.
    
    Se o número for maior que zero, exiba a mensagem "O número é positivo." Se for menor que zero, exiba "O número é negativo." Caso seja zero, a mensagem deve ser "O número é zero."
    
    Utilize estruturas condicionais e formatação com F-string para criar o programa de forma clara.

---
+++ Prova 2 - Condicionais.py
@@ -0,0 +1,8 @@
+numero = float(input('Insira um numero: '))
+
+if numero > 0:
+    print(f'O número {numero} é Positivo.')
+elif numero < 0:
+    print(f'O número {numero} é Negativo')
+else:
+    print(f'O número {numero} é zero.')
