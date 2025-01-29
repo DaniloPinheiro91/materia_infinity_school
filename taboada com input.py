367efbf40b8ccd23d9f849de45b0655e08b468b2
DaniloPinheiro91 <dpinheiro.profissional@hotmail.com>
2025-01-29 07:50:00 -0300

    atividade 3 - Aula 3 - Tabuada de um Número:
    Faça um programa que solicite um número ao usuário e use
    um laço while para exibir a tabuada desse número (de 1 a 10).

---
+++ taboada com input.py
@@ -0,0 +1,6 @@
+numero = int(input('Qual numero você quer saber a taboada? '))
+numero2 = 0
+while numero2 <10:
+    resultado = numero * numero2
+    print(f'{numero2} X {numero} = {resultado}' )
+    numero2 +=1
