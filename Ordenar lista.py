1a784d56e1fef187502f3082e574b2464d6f095a
DaniloPinheiro91 <dpinheiro.profissional@hotmail.com>
2025-02-07 14:42:18 -0300

    GPT Sugestão/Treino - Ordenar a lista
    Crie uma lista de Strings e ordene-a em ordem alfabética. Em seguida, imprima a lista ordenada.

---
+++ Ordenar lista.py
@@ -0,0 +1,7 @@
+lista_de_string = ['beterraba', 'amora', 'goiaba', 'ameixa', 'coco',
+                    'jaca', 'laranja', 'mexirica' ]
+
+print(f'Lista inicial: {lista_de_string}\n')
+
+lista_de_string.sort()
+print(f'Lista Ordenada: {lista_de_string}')
