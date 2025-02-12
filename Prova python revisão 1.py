229257f9d394a21c401515d0d4e4a948a0a27d08
DaniloPinheiro91 <dpinheiro.profissional@hotmail.com>
2025-02-12 13:58:03 -0300

    Prova PYIA - Revisão Geral I
    Crie um dicionário para armazenar o nome e o preço de cinco produtos. Peça ao usuário para inserir o nome de cada produto e o respectivo preço. À medida que o usuário fornece os dados, armazene cada produto como uma chave no dicionário e o preço como o valor associado a essa chave. Após a inserção de todos os produtos e preços, calcule o valor total da compra somando todos os preços armazenados no dicionário. Por fim, exiba o valor total da compra.

---
+++ Prova python revisão 1.py
@@ -0,0 +1,15 @@
+produtos = {}
+
+for i in range(5):
+    nome_produto = input(f"Digite o nome do produto {i + 1}: ")
+    preco_produto = float(input(f"Digite o preço do produto {nome_produto}: R$ "))
+    produtos[nome_produto] = preco_produto
+valor_total = sum(produtos.values())
+
+print('-='*30)
+print("\nResumo da compra:")
+
+for produto, preco in produtos.items():
+    print(f"{produto}: R$ {preco:.2f}")
+
+print(f"\nValor total da compra: R$ {valor_total:.2f}")# Escreva o seu código aqui :-)
